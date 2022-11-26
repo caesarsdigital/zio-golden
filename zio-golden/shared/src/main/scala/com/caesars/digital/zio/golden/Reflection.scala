f7ec0cd851c04d5890343f51b9caa7a6bfb0873d
alex.kooper@gmail.com <alex.kooper@gmail.com>
2022-11-26 15:07:58 -0500

    Add comments about reflection

--- zio-golden/shared/src/main/scala/com/caesars/digital/zio/golden/Reflection.scala
+++ zio-golden/shared/src/main/scala/com/caesars/digital/zio/golden/Reflection.scala
@@ -11,10 +11,15 @@ object Reflection {
   }
 
   def typePackage[A: Tag]: Seq[String] = {
-    val r = typeNameParts[A].head.split('.').toSeq
-    r.take(r.length - 1)
+    val packageAndClassName = typeNameParts[A].head.split('.').toSeq
+    packageAndClassName.take(packageAndClassName.length - 1)
   }
 
+  /** @param tag
+    *   zio.Tag that allows to do reflection using izumi.reflect.macrortti.LightTypeTag
+    * @return
+    *   representation of type as a String in the following format: 'com.package1.package2.Object1::Object2::ClassName'
+    */
   private def typeRepr[A](implicit tag: Tag[A]): String = tag.tag.repr
 
   private def typeNameParts[A: Tag]: Seq[String] = typeRepr[A].split("::").toSeq
